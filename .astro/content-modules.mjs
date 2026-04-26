35eee648cc85270463649f557d5395c389b54666
camomille <camicristdosantos@gmail.com>
2026-04-26 14:29:16 -0300

    illustration in the profile area

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/work/rd-station.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Frd-station.mdx&astroContentModuleFlag=true")],
 ["src/content/work/next-case.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fnext-case.mdx&astroContentModuleFlag=true")],
+["src/content/work/rd-station.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Frd-station.mdx&astroContentModuleFlag=true")],
 ["src/content/work/unholy-women.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Funholy-women.mdx&astroContentModuleFlag=true")],
 ["src/content/work/will-bank.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fwill-bank.mdx&astroContentModuleFlag=true")]]);
 		
